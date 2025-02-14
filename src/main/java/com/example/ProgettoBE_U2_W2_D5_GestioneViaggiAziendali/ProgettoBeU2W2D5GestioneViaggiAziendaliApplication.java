5d574f29abef6135e2468df61ec261cac41ee5fb
Edoardo <edfl.tunzi@gmail.com>
2025-02-14 11:22:13 +0100

    initial commit

---
+++ src/main/java/com/example/ProgettoBE_U2_W2_D5_GestioneViaggiAziendali/ProgettoBeU2W2D5GestioneViaggiAziendaliApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProgettoBE_U2_W2_D5_GestioneViaggiAziendali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoBeU2W2D5GestioneViaggiAziendaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoBeU2W2D5GestioneViaggiAziendaliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProgettoBE_U2_W2_D5_GestioneViaggiAziendali/ProgettoBeU2W2D5GestioneViaggiAziendaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProgettoBE_U2_W2_D5_GestioneViaggiAziendali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoBeU2W2D5GestioneViaggiAziendaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
