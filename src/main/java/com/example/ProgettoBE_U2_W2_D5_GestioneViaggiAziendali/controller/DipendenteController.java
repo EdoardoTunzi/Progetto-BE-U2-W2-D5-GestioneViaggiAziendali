4c3652e6cc4055f6654784b0d80d9546ca1558a7
Edoardo <edfl.tunzi@gmail.com>
2025-02-14 19:05:26 +0100

    basic CRUD in Dipendente controller added and working

--- src/main/java/com/example/ProgettoBE_U2_W2_D5_GestioneViaggiAziendali/controller/DipendenteController.java
+++ src/main/java/com/example/ProgettoBE_U2_W2_D5_GestioneViaggiAziendali/controller/DipendenteController.java
@@ -1,13 +1,68 @@
 package com.example.ProgettoBE_U2_W2_D5_GestioneViaggiAziendali.controller;
 
+import com.example.ProgettoBE_U2_W2_D5_GestioneViaggiAziendali.dto.DipendenteDTO;
 import com.example.ProgettoBE_U2_W2_D5_GestioneViaggiAziendali.service.DipendenteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/dipendente")
 public class DipendenteController {
     @Autowired
     DipendenteService dipendenteService;
+
+    @GetMapping(value = "", produces = "application/json")
+    public ResponseEntity<Page<DipendenteDTO>> getAllDipendenti(Pageable page) {
+        Page<DipendenteDTO> dipendenti = dipendenteService.getAllDipendenti(page);
+        return new ResponseEntity<>(dipendenti, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public DipendenteDTO getDipendenteById(@PathVariable long id) {
+        return dipendenteService.findDipendenteById(id);
+
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> nuovoDipendente(@RequestBody @Validated DipendenteDTO dipendenteDTO, BindingResult validation) {
+
+        if (validation.hasErrors()) {
+            String messaggioErrori = "ERRORE DI VALIDAZIONE \n";
+
+            for (ObjectError errore : validation.getAllErrors()) {
+                messaggioErrori += errore.getDefaultMessage() + "\n";
+            }
+            return new ResponseEntity<>(messaggioErrori, HttpStatus.BAD_REQUEST);
+        }
+
+        Long idNuovoDipendente = dipendenteService.saveDipendente(dipendenteDTO);
+        return new ResponseEntity<>("Viaggio inserito nel DB con id: " + idNuovoDipendente, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/modifica/{id}")
+    public ResponseEntity<?> updateDipendente(@RequestBody @Validated DipendenteDTO dipendenteDTO,BindingResult validation, @PathVariable long id ) {
+        if (validation.hasErrors()) {
+            String messaggioErrori = "ERRORE DI VALIDAZIONE \n";
+
+            for (ObjectError errore : validation.getAllErrors()) {
+                messaggioErrori += errore.getDefaultMessage() + "\n";
+            }
+            return new ResponseEntity<>(messaggioErrori, HttpStatus.BAD_REQUEST);
+        } else {
+            dipendenteService.updateDipendente(dipendenteDTO, id);
+            return new ResponseEntity<>("Il dipendente è stato modificato correttamente", HttpStatus.OK);
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteDipendente(@PathVariable long id) {
+        return new ResponseEntity<>(dipendenteService.deleteDipendente(id), HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/ProgettoBE_U2_W2_D5_GestioneViaggiAziendali/service/DipendenteService.java
+++ src/main/java/com/example/ProgettoBE_U2_W2_D5_GestioneViaggiAziendali/service/DipendenteService.java
@@ -1,11 +1,109 @@
 package com.example.ProgettoBE_U2_W2_D5_GestioneViaggiAziendali.service;
 
+import com.example.ProgettoBE_U2_W2_D5_GestioneViaggiAziendali.dto.DipendenteDTO;
+import com.example.ProgettoBE_U2_W2_D5_GestioneViaggiAziendali.dto.ViaggioDTO;
+import com.example.ProgettoBE_U2_W2_D5_GestioneViaggiAziendali.model.Dipendente;
+import com.example.ProgettoBE_U2_W2_D5_GestioneViaggiAziendali.model.Viaggio;
 import com.example.ProgettoBE_U2_W2_D5_GestioneViaggiAziendali.repository.DipendenteDAORepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DipendenteService {
     @Autowired
     DipendenteDAORepository dipendenteRepo;
+    //salvataggio
+    public Long saveDipendente(DipendenteDTO dipendenteDTO) {
+        Dipendente dipendenteInserito = fromDipendenteDTOToDipendente(dipendenteDTO);
+        Dipendente dipendenteSalvato = dipendenteRepo.save(dipendenteInserito);
+        return dipendenteSalvato.getId();
+    }
+
+    //getAll
+    public Page<DipendenteDTO> getAllDipendenti(Pageable page) {
+        Page<Dipendente> listaDipendenti = dipendenteRepo.findAll(page);
+        List<DipendenteDTO> listaDto = new ArrayList<>();
+
+        for (Dipendente dipendente : listaDipendenti.getContent()) {
+            DipendenteDTO dto = fromDipendenteToDipendenteDTO(dipendente);
+            listaDto.add(dto);
+        }
+
+        Page<DipendenteDTO> listaPage = new PageImpl<>(listaDto);
+        return listaPage;
+    }
+
+    //getById
+    public DipendenteDTO findDipendenteById(long id) {
+        Optional<Dipendente> dipendente = dipendenteRepo.findById(id);
+
+        if (dipendente.isPresent()) {
+            DipendenteDTO dipendenteDto = fromDipendenteToDipendenteDTO(dipendente.get());
+            return dipendenteDto;
+        } else {
+            throw new RuntimeException("Nessun dipendente trovato con l'id inserito");
+        }
+    }
+    //put- sostituisciDipendenteById
+    public void updateDipendente(DipendenteDTO dipendenteDTO, long id) {
+        Optional<Dipendente> dipendenteTrovato = dipendenteRepo.findById(id);
+
+        if(dipendenteTrovato.isPresent()) {
+            Dipendente dipendente = dipendenteTrovato.get();
+            dipendente.setUsername(dipendenteDTO.getUsername());
+            dipendente.setNome(dipendenteDTO.getNome());
+            dipendente.setCognome(dipendenteDTO.getCognome());
+            dipendente.setEmail(dipendenteDTO.getEmail());
+            dipendente.setProfilePic(dipendenteDTO.getProfilePic());
+            dipendenteRepo.save(dipendente);
+        } else {
+            throw new RuntimeException("Errore nella modifica del dipendente inserito");
+        }
+    }
+
+    //deleteByID
+    public String deleteDipendente(long id) {
+        Optional<Dipendente> dipendenteTrovato = dipendenteRepo.findById(id);
+        if (dipendenteTrovato.isPresent()){
+            dipendenteRepo.delete(dipendenteTrovato.get());
+            return "Dipendente con id: " + id + " eliminato con successo!";
+        } else {
+            throw new RuntimeException("Errore nel delete! Nessun dipendente trovato con questo id");
+        }
+
+    }
+
+
+
+
+    //--------------------------metodi travaso DTO----------------------
+
+    //Dipendente to DipendenteDTO
+    public Dipendente fromDipendenteDTOToDipendente(DipendenteDTO dipendenteDTO) {
+        Dipendente dipendente = new Dipendente();
+        dipendente.setUsername(dipendenteDTO.getUsername());
+        dipendente.setNome(dipendenteDTO.getNome());
+        dipendente.setCognome(dipendenteDTO.getCognome());
+        dipendente.setEmail(dipendenteDTO.getEmail());
+        dipendente.setProfilePic(dipendenteDTO.getProfilePic());
+        return dipendente;
+    }
+
+    //DipendenteDTO to Dipendente
+    public DipendenteDTO fromDipendenteToDipendenteDTO(Dipendente dipendente) {
+        DipendenteDTO dipendenteDTO = new DipendenteDTO();
+        dipendenteDTO.setUsername(dipendente.getUsername());
+        dipendenteDTO.setNome(dipendente.getNome());
+        dipendenteDTO.setCognome(dipendente.getCognome());
+        dipendenteDTO.setEmail(dipendente.getEmail());
+        dipendenteDTO.setProfilePic(dipendente.getProfilePic());
+        return dipendenteDTO;
+    }
 }
